574e9f1a881a58e529276de8500e1a9c27c6629f
ivanpires2000 <ivan.pires2000@gmail.com>
2025-04-24 21:59:36 +0000

    feat: reformulação do layout e melhorias no formulário de contato

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 // Validação simples do formulário
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Impede o envio padrão
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Impede o envio padrão
 
-    const name = document.getElementById("name").value.trim();
-    const phone = document.getElementById("phone").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
+  const name = document.getElementById('name').value.trim();
+  const phone = document.getElementById('phone').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const message = document.getElementById('message').value.trim();
 
-    if (!name || !phone || !email || !message) {
-      alert("Por favor, preencha todos os campos.");
-      return;
-    }
+  if (!name || !phone || !email || !message) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
 
-    // Simulação de envio
-    alert("Mensagem enviada com sucesso!");
-    this.reset(); // Limpa o formulário
-  });
+  // Simulação de envio
+  alert('Mensagem enviada com sucesso!');
+  this.reset(); // Limpa o formulário
+});
